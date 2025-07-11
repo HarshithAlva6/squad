9b7e20c4fe0fdb47991e6e2bb23027e014ba3a34
Harshith <harshalva.titan@csu.fullerton.edu>
2025-07-11 14:07:04 -0700

    Completed Changes with oout folder

--- squad/next.config.ts
+++
@@ -1,9 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export', 
-  basePath: process.env.NODE_ENV === 'production' ? '/squad/squad' : '', 
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/squad/squad' : '', 
-};
-
-export default nextConfig;

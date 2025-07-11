7f6c572dbcb8f622334ccafb8d29e2d12833607e
Harshith <harshalva.titan@csu.fullerton.edu>
2025-07-11 13:47:26 -0700

    Rectified next config file

--- squad/next.config.ts
+++ squad/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export', 
-  basePath: process.env.NODE_ENV === 'production' ? '/squad' : '', 
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/squad/' : '', 
+  basePath: process.env.NODE_ENV === 'production' ? '/squad/squad' : '', 
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/squad/squad' : '', 
 };
 
 export default nextConfig;
